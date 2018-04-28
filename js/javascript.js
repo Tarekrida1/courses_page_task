018be3681c13cac531b0ba6bf5be59f09dd08e07
TaReK RiDa <37909439+Tarekrida1@users.noreply.github.com>
2018-04-28 10:09:44 +0200

    update1

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,3 @@
-
-window.onload = function() {
-    document.getElementById('loading').style.display = 'none';
-  };
-
 function logout() {
     document.getElementById("logout").innerHTML = "Login"
     document.getElementById("user-ahmed").innerHTML = ""
