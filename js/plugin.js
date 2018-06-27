5a25933da49538025e4d4e0db3f441ede46f342c
TaReK RiDa <37909439+Tarekrida1@users.noreply.github.com>
2018-06-27 15:35:04 +0200

    update
    up

--- js/plugin.js
+++ js/plugin.js
@@ -9,7 +9,7 @@ $(function () {
         if ($(document).scrollTop() > 200) { 
             $(".main-header").css("box-shadow", "0 0rem 1rem 0rem");
             $(".main-header").css({
-                background: "#0097A7"
+                background: "#fff"
                                     });
         } else {
             $(".main-header").css("box-shadow", "0 0 0 0");
@@ -24,15 +24,15 @@ $(function () {
     });
     // nav bar
     $(".open-nav").on('click', function () {
-        $(".fixed-option").animate({
-            left: 0,
+        $(".fixed-option").css({
+            transform: "translateX(0)",
             opacity: 1
-                                     }, 800);
+                                     });
                                           });
     $(".close-nav").on('click', function () {
-        $(".fixed-option").animate({
-            left: "-240",
-            opacity: 0
+        $(".fixed-option").css({
+            transform: "translateX(-100%)",
+            opacity: .3
                                     }, 800);
                                          });
 
@@ -106,4 +106,101 @@ $(window).load(function(){
             });
         });
     });
+
+
+
+/*
+// get the click li  id and cache in myId varible
+ $("#my-tabs li")./click(function()
+ {
+var myID = $(this).attr("id");
+// remove class inactive frome clicked li and add it to other li in the same UL
+$(this).removeClass("inactive").siblings().addClass("inactive");
+$(".the-tabs div").hide();
+// hide all divs when click on the li
+$("#"+myID+"-content").fadeIn(1500);
+});
+// tabs
+
+*/
+$(".grades .table a, .signup .not-have a, .login-out-user").on("click", function(even){
+    event.preventDefault();
+$("." + $(this).data("popup")).fadeIn(300);
+$("body").css({
+    overflow: "hidden"
+});
+});
+$(".close-popup, .login-content .close-login").on("click", function(event) {
+    event.preventDefault();
+$(".popup").fadeOut(300);
+$("body").css({
+    overflow: "auto"
+});
+});
+$(".close-popup, .login-content .close-login").on("click", function(event) {
+    event.preventDefault();
+$(".popup").fadeOut(300);
+$("body").css({
+    overflow: "auto"
+});
+});
+
+$(".message-click").on("click", function() {
+    event.preventDefault();
+    $(".message").fadeIn();
+    $("body").css({
+        overflow: "hidden"
+    });
+});
+
+$(".close-message").on("click", function() {
+    event.preventDefault();
+    $(".message").fadeOut();
+    $("body").css({
+        overflow: "auto"
+    });
+});
+// inputs 
+$(".first-name input").on("focusout", function() {
+if ($(this).val() != "") {
+    $(".first-name label").addClass("has-data");
+} else {
+    $(".first-name label").removeClass("has-data");
+}
+});
+$(".last-name input").on("focusout", function() {
+    if ($(this).val() != "") {
+        $(".last-name label").addClass("has-data");
+    } else {
+        $(".last-name label").removeClass("has-data");
+    }
+    });
+    $(".email input").on("focusout", function() {
+        if ($(this).val() != "") {
+            $(".email label").addClass("has-data");
+        } else {
+            $(".email label").removeClass("has-data");
+        }
+        });
+        $(".phone input[type=number]").on("focusout", function() {
+            if ($(this).val() != "") {
+                $(".phone label").addClass("has-data");
+            } else {
+                $(".phone label").removeClass("has-data");
+            }
+            });
+            $(".password input").on("focusout", function() {
+                if ($(this).val() != "") {
+                    $(".password label").addClass("has-data");
+                } else {
+                    $(".password label").removeClass("has-data");
+                }
+                });
+                $(".repassword input").on("focusout", function() {
+                    if ($(this).val() != "") {
+                        $(".repassword label").addClass("has-data");
+                    } else {
+                        $(".repassword label").removeClass("has-data");
+                    }
+                    });
 });
